c689dd433407a85c8c5635246f6f694ca52199d3
Yeregorix <yeregorixcraft@gmail.com>
2024-10-21 20:48:33 +0200

    Update to SpongeAPI 12

--- src/main/java/net/smoofyuniverse/ore/update/UpdateChecker.java
+++ src/main/java/net/smoofyuniverse/ore/update/UpdateChecker.java
@@ -69,7 +69,7 @@ public class UpdateChecker {
 						 String owner, String name, String spongeDependency) {
 		this(logger, plugin, loader,
 				new OreProject(plugin.metadata().id()),
-				v -> v.dependencies.get(spongeDependency).startsWith("11."),
+				v -> v.dependencies.get(spongeDependency).startsWith("12."),
 				plugin.metadata().id() + ".update.notify");
 		if (owner == null || owner.isEmpty())
 			throw new IllegalArgumentException("owner");
